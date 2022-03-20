3d0303aa9f42bbe82a6998614e733752507ea16e
Rokas Balsys <rokas.balsys@ngenius.ai>
2022-03-20 15:05:20 +0200

    update changes to 4th tutorial

--- 04_Django-custom-users/django_website/settings.py
+++ 04_Django-custom-users/django_website/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Add our new application
+    'users',
     'main',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
