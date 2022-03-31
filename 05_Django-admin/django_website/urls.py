931865fd4d5f5f2e45333e35d9673e8c3068b17b
Rokas Balsys <rokas.balsys@ngenius.ai>
2022-03-31 11:57:57 +0300

    finish 5th tutorial

--- 05_Django-admin/django_website/urls.py
+++ 05_Django-admin/django_website/urls.py
@@ -19,8 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include('main.urls')),
+    path('tinymce/', include('tinymce.urls')),
     path('admin', admin.site.urls),
+    path("", include('main.urls')),
 ]
 
 if settings.DEBUG:
--- 05_Django-admin/main/models.py
+++ 05_Django-admin/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from tinymce.models import HTMLField
 
 # Create your models here.
 class ArticleSeries(models.Model):
@@ -8,24 +9,28 @@ class ArticleSeries(models.Model):
     slug = models.SlugField("Series slug", null=False, blank=False, unique=True)
     published = models.DateTimeField('Date published', default=timezone.now)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         # otherwise we get "Article Series" in admin panel
-        verbose_name_plural = "Series"
         ordering = ['-published']
+        verbose_name_plural = "Series"
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200, default='', blank=True)
     slug = models.SlugField("Series slug", null=False, blank=False, unique=True)
-    content = models.TextField()
+    content = HTMLField(blank=True, default="")
+    notes = HTMLField(blank=True, default="")
     published = models.DateTimeField('Date published', default=timezone.now)
     modified = models.DateTimeField('Date modified', default=timezone.now)
-    # series = models.ForeignKey(ArticleSeries, default='', verbose_name="Series", on_delete=models.SET_DEFAULT)
+    series = models.ForeignKey(ArticleSeries, default='', verbose_name="Series", on_delete=models.SET_DEFAULT)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['-published']
-        verbose_name_plural = "Article"
+        verbose_name_plural = "Articles"
